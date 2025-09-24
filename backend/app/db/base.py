5f6e3a61001b2c39390a54d7360c52c37fc8ceab
AmirUpSkill <amirabdallahpfe@gmail.com>
2025-09-24 10:47:06 +0100

    feat : build db layer

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,1 +1,4 @@
-.  
+from sqlalchemy.orm import declarative_base
+
+# --- Base Class For ORM --- 
+Base = declarative_base()
--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,1 +1,16 @@
-.  
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.config import settings
+
+# --- Create the SQLAlchemy engine ---
+engine = create_engine(settings.DB_URL, pool_pre_ping=True)
+# --- Create Local Session ---
+SessionLocal = sessionmaker(autocommit=False , autoflush=False)
+# --- Get DB --- 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
