78b93032d54d8936dc3e73748e0a26e8793cbe23
AmirUpSkill <amirabdallahpfe@gmail.com>
2025-09-24 11:47:24 +0100

    feat : build user schema

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,1 +1,27 @@
-.  
+import uuid 
+from datetime import datetime 
+from pydantic import BaseModel 
+
+# --- Base Schema --- 
+class UserBase(BaseModel):
+    """
+        Base Schema for a User 
+    """
+    email: str 
+    name: str 
+    avatar_url: str | None = None 
+
+class UserCreate(UserBase):
+    pass 
+
+# --- Read/Response Schema --- 
+class User(UserBase):
+    """
+        Schema for returning a user form the API 
+    """
+    id: uuid.UUID
+    created_at: datetime 
+    # --- Config --- 
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
